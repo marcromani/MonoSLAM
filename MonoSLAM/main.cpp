b13682032ca83913db121b2570ecdfe7d01751e8
Marc Romaní <marcromani.ub@gmail.com>
2017-04-09 14:20:57 +0200

    Added findCandidates: looks for features candidates in an admissible region

--- MonoSLAM/main.cpp
+++ MonoSLAM/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,6 +21,7 @@ bool getOptions(int argc, char *argv[], string& cameraFile, int& patternRows, in
 void printUsage(const char *execName);
 
 void drawFeatures(const Mat& frame, const vector<Feature>& features);
+Mat findCandidates(const Mat& frame, const vector<Feature>& features);
 
 int main(int argc, char *argv[]) {
 
@@ -98,6 +100,21 @@ int main(int argc, char *argv[]) {
 
         cap.read(frame);
 
+        cvtColor(frame, gray, CV_BGR2GRAY);
+
+        drawFeatures(frame, map.features);
+
+        Mat corners = findCandidates(gray, map.features);
+
+        // Draw a circle around corners
+        for (int j = 0; j < corners.rows; j++) {
+            for (int i = 0; i < corners.cols; i++) {
+
+                Point p = corners.at<Point2f>(j, i);
+                circle(frame, p, 5, Scalar(0, 0, 255), 1, CV_AA, 0);
+            }
+        }
+
         imshow(window, frame);
 
         if (waitKey(1) == 27)
@@ -147,4 +164,48 @@ void drawFeatures(const Mat& frame, const vector<Feature>& features) {
 
         circle(frame, Point2i(x, y), 5, Scalar(0, 0, 255), 1, CV_AA, 0);
     }
+}
+
+Mat findCandidates(const Mat& frame, const vector<Feature>& features) {
+
+    Mat corners;
+
+    int maxCorners = 15 - features.size();
+    double qualityLevel = 0.2;
+    double minDistance = 60;
+
+    Mat mask(frame.size(), CV_8UC1, Scalar(0));
+
+    // Set a margin around the mask to enclose the detected corners inside
+    // a rectangle and avoid premature corner loss due to camera movement
+    int pad = 30;
+    mask(Rect(pad, pad, mask.cols - 2 * pad, mask.rows - 2 * pad)).setTo(Scalar(255));
+
+    // Compute the additional length that a feature patch must have
+    // so that new detected corners are the same minimum distance apart
+    // from already initialized features than from themselves
+    int len = 2 * minDistance - features[0].roi.width;
+
+    for (unsigned int i = 0; i < features.size(); i++) {
+
+        Rect roi = features[i].roi;
+
+        roi.x = max(0, roi.x - len / 2);
+        roi.y = max(0, roi.y - len / 2);
+
+        roi.width += len;
+        roi.height += len;
+
+        if (roi.x + roi.width > frame.cols)
+            roi.width = frame.cols - roi.x;
+        if (roi.y + roi.height > frame.rows)
+            roi.height = frame.rows - roi.y;
+
+        // Update the mask to reject the region around the ith feature
+        mask(roi).setTo(Scalar(0));
+    }
+
+    goodFeaturesToTrack(frame, corners, maxCorners, qualityLevel, minDistance, mask);
+
+    return corners;
 }
